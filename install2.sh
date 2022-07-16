3ef4ad8632d5654dd4c8d071c692381c74f77b2a
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2022-07-16 10:54:03 +0530

    Add files via upload

---
+++ install2.sh
@@ -0,0 +1,6 @@
+#pip3 install numpy==1.19.2
+#pip3 install tensorflow-2.6.0-cp39-cp39-linux_aarch64.whl
+#pip3 install cvlib
+#pip3 install keras==2.6.0
+#apt-get install libatlas-base-dev -y
+wget https://pjreddie.com/media/files/yolov3.weights
